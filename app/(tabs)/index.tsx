438ec30d576cd38df9713718c9f5112129372d1d
LoickBde <loick.baude@gmail.com>
2025-06-13 23:42:52 +0200

    chore: styling navbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className={"flex-1 justify-center items-center"}>
-      <Text className="text-5xl text-dark-200 text-bold">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View>
+      <Text>Home</Text>
     </View>
   );
 }
